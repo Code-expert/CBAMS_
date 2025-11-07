78993690efb1d701592bfe11b23ae861f02a617f
Om6124 <omnathganapure9981@gmail.com>
2025-11-07 16:42:09 +0530

    frontend for prod

--- cbams-frontend/vite.config.js
+++ cbams-frontend/vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+  preview: {
+    allowedHosts: ['https://cbams-1.onrender.com/'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 10000
+  }
 })
